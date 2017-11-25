1da20a6be87c2bde5b3d04ea2607ee6e3b79aed8
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2017-11-25 18:49:05 +0000

    Initial commit

---
+++ src/myHelloWorldVSCoreLambda/Index.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace myHelloWorldVSCoreLambda
+{
+    public class Index
+    {
+        public Stream MyHandler(Stream stream)
+        {
+            //function logic
+        }
+    }
+}
+
+
